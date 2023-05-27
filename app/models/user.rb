7e7926401659b90af6bca0fcd3c3b2fc95ac8767
ayapeco <be.mw-ke.mp@f3.dion.ne.jp>
2023-05-27 21:16:32 +0900

    Validation＆DB修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
   validates :family_name_kana,   presence: true
   validates :nickname,           presence: true
   validates :birthday,           presence: true
-  has_many :buying_records
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2023_05_21_083616) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "first_name", null: false
     t.string "family_name", null: false
     t.string "first_name_kana", null: false
