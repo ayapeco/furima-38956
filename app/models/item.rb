bb4968e799a60d1b923efd70c5eab288a4a4ba8d
ayapeco <be.mw-ke.mp@f3.dion.ne.jp>
2023-05-27 22:08:57 +0900

    item テーブルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord- has_one :buying_record
+  belong_to :user
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :shipping_region
+  belongs_to_active_hash :shipping_time
+end
