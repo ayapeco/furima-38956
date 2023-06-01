c73ca857999e68f92bff8f3c017661b4474319e8
ayapeco <be.mw-ke.mp@f3.dion.ne.jp>
2023-06-01 23:18:46 +0900

    JSコードの作成

---
+++ app/javascript/item_price.js
@@ -0,0 +1,16 @@
+//window.addEventListener('load', () => {
+  //const priceInput = document.getElementById("item-price");
+  //priceInput.addEventListener("input", () => {
+    //console.log("イベント発火");
+  //})
+//});
+
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+  const addPofitDom = document.getElementById("profit");
+   addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+ })
+});
