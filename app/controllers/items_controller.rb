8e1c81d0ffc04abc30b95f6375ee5104e4a08316
ayapeco <be.mw-ke.mp@f3.dion.ne.jp>
2023-06-04 19:01:27 +0900

    レヴュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_item, only: [:edit, :show, :update]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -9,14 +11,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to action: :index
   end
+ end
 
   def update
     @item = Item.find(params[:id])
@@ -28,7 +29,7 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-end
+
 
   def create
     @item = Item.new(item_params)
@@ -39,9 +40,8 @@ end
       render :new
     end
   end
-end
 
-  private
+private
 
 def item_params
   params.require(:item).permit(:items_name, :items_description, :category_id, :condition_id, :shipping_fee_id,
@@ -50,4 +50,9 @@ end
 
 def move_to_index
   redirect_to action: :index unless user_signed_in?
+end
+
+def set_item
+  @item = Item.find(params[:id])
+end
 end
